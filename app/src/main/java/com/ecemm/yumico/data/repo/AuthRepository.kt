2ece635ddfb717fb5b65607b2457e9a1db6dc948
Ecem <ecemnazgorusuk@gmail.com>
2025-10-10 15:14:24 +0300

    rearranged

---
+++ app/src/main/java/com/ecemm/yumico/data/repo/AuthRepository.kt
@@ -0,0 +1,4 @@
+package com.ecemm.yumico.data.repo
+//Firebase Auth & Firestore işlemleri
+class AuthRepository {
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt) apply false //  Hilt plugin
-   // id("com.google.gms.google-services") version "4.4.3" apply false //firestore
+    // id("com.google.gms.google-services") version "4.4.3" apply false //firestore
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false // map
 }
